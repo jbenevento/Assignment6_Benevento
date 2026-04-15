0ff9e1463bf7b0dead92dbe380f850537babe7e3
jessb <beneventojessica@gmail.com>
2026-04-14 20:52:30 -0500

    Completed AllLess class

---
+++ src/AllLess.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class AllLess {
+    public static void main(String[] args) {
+        String[] string = {
+                "zero", " size", "nutella", "jojo", "luna",
+                "isse", "astor", "as", "entretien","", "cal"
+        };
+
+        int x = 3;
+
+        List<String> result = findAllLess(string, x);
+        System.out.println(result);
+    }
+
+    public static List<String> findAllLess(String[] string, int x) {
+        List<String> result = new ArrayList<>();
+
+        // If array is empty, return empty list
+        if(string == null) return result;
+
+        for(String s : string) {
+
+            // Skip if null or empty
+            if(s == null) continue;
+
+            // Add string to array if not empty and less than x
+            if (!s.equals("") && s.length() < x) {
+                result.add(s);
+            }
+        }
+
+        return result;
+
+    }
+
+}
+
+/*
+Sources:
+https://www.geeksforgeeks.org/dsa/binary-heap/
+https://docs.oracle.com/javase/8/docs/api/java/lang/String.html
+
+ */
---
+++ src/EncryptedBinaryTree.java
@@ -0,0 +1,52 @@
+public class EncryptedBinaryTree {
+    public static void main(String[] args) {
+
+        int[] bt = {-2, -2, -1, -2, -1};
+        int t = 1;
+
+        EncryptedBinaryTree solution = new EncryptedBinaryTree();
+
+        boolean result = solution.findTarget(bt, t);
+
+        System.out.println(result);
+    }
+
+    public boolean findTarget(int[] bt, int target) {
+        // Start DFS at
+        // index = 0 (root of array tree)
+        // value = 1 (root value)
+        return dfs(bt, 0, 1, target);
+    }
+
+    private boolean dfs(int[] bt, int i, int value, int target) {
+
+        // Base cases:
+        // If index out of bounds (no node exists) or
+        // node is marked -1 (empty node)
+        if (i >= bt.length || bt[i] == -1) {
+            return false;
+        }
+
+        // Check if current value matches target
+        if (value == target) {
+            return true;
+        }
+
+        // Compute left and right values:
+        // left = 3x + 1; right = 2x + 5
+        int leftValue = 3 * value + 1;
+        int rightValue = 2 * value + 5;
+
+        // Recursively search:
+        // left child is at index 2 * i + 1
+        // right child is at index 2 * i + 2
+        return dfs(bt, 2 * i + 1, leftValue, target)
+                || dfs(bt, 2 * i + 2, rightValue, target);
+    }
+}
+
+/*
+Sources:
+https://www.geeksforgeeks.org/java/implementing-a-binary-tree-in-java/
+https://stackoverflow.com/questions/20756417/recursive-tree-encryption-method-using-dfs
+ */
